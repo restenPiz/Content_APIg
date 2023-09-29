77912673ce8db22fef52a82099ed4c39839a8520
restenPiz <99766054+restenPiz@users.noreply.github.com>
2023-09-29 14:22:09 +0200

    Some updates in the application

--- content_api/app/Http/Controllers/fileController.php
+++ content_api/app/Http/Controllers/fileController.php
@@ -4,9 +4,11 @@ namespace App\Http\Controllers;
 
 use App\Models\Blog;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 
 class fileController extends Controller
 {
+    //*Inicio do metodo para salvar os dados
     public function storeFile(Request $request)
     {
         $contents=new Blog();
@@ -31,5 +33,31 @@ class fileController extends Controller
 
         //*Inicio do metodo para fazer o redirecionamento
         return response();
+    }
+    //*Inicio do metodo para fazer o update dos dados
+    public function updateFile($id, Request $request)
+    {
+        //*Recuperando os dados usando o id
+        $contents=Blog::findOrFail($id);
+
+        $contents->Title=$request->input('Title');
+        $contents->Title_blog=$request->input('Title_blog');
+        $contents->Subtitle_blog=$request->input('Subtitle_blog');
+        $contents->Category=$request->input('Category');
+        $local=$contents->Another;
+
+        //?Inicio do metodo que e responsavel por eliminar uma imagem ja existente
+        if (File::exists($local)) {
+            File::delete($local);
+        }
+
+        //?Inicio do metodo que e responsavel por salvar uma imagem
+        if ($request->file('Another')->isValid()) {
+            $filename = $request->file('Another')->getClientOriginalName();
+            $link = "Ficheiros/" . $filename;
+            $contents->Another = $link;
+            $foto = $request->file('Another');
+            $foto->move('Ficheiros/', $filename);
+        }
     }
 }
