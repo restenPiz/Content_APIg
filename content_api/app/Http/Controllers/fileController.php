8cbfac00fa14295de79719337946ecee5a8483b4
restenPiz <99766054+restenPiz@users.noreply.github.com>
2023-09-29 11:04:53 +0200

    Some updates in the application

--- content_api/app/Http/Controllers/fileController.php
+++ content_api/app/Http/Controllers/fileController.php
@@ -2,12 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Blog;
 use Illuminate\Http\Request;
 
 class fileController extends Controller
 {
     public function storeFile()
     {
-        
+        $contents=new Blog();
+
+        //*Requisitando todos os dados do request
     }
 }
