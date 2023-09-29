d7761c43b77b0819be15c7e2438470d4f5d0de2a
restenPiz <99766054+restenPiz@users.noreply.github.com>
2023-09-29 14:14:26 +0200

    Some updates in the application

--- content_api/app/Http/Controllers/fileController.php
+++ content_api/app/Http/Controllers/fileController.php
@@ -7,10 +7,23 @@ use Illuminate\Http\Request;
 
 class fileController extends Controller
 {
-    public function storeFile()
+    public function storeFile(Request $request)
     {
         $contents=new Blog();
 
         //*Requisitando todos os dados do request
+        $contents->Title=$request->input('Title');
+        $contents->Title_blog=$request->input('Title_blog');
+        $contents->Subtitle_blog=$request->input('Subtitle_blog');
+        $contents->Category=$request->input('Category');
+
+        //?Inicio do metodo responsavel por fazer a insercao da imagem
+
+
+        //*Inicio do metodo para fazer a insercao dos dados na base de dados
+        $contents->save();
+
+        //*Inicio do metodo para fazer o redirecionamento
+        return response();
     }
 }
