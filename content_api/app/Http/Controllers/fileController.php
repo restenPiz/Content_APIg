e5002f8ec107947c731bb7f54cc60dbc481472d2
restenPiz <99766054+restenPiz@users.noreply.github.com>
2023-09-29 14:16:26 +0200

    Some updates in the application

--- content_api/app/Http/Controllers/fileController.php
+++ content_api/app/Http/Controllers/fileController.php
@@ -18,7 +18,13 @@ class fileController extends Controller
         $contents->Category=$request->input('Category');
 
         //?Inicio do metodo responsavel por fazer a insercao da imagem
-
+        if ($request->file('Another')->isValid()) {
+            $filename = $request->file('Another')->getClientOriginalName();
+            $link = "Ficheiros/" . $filename;
+            $contents->Another = $link;
+            $foto = $request->file('Another');
+            $foto->move('Ficheiros/', $filename);
+        }
 
         //*Inicio do metodo para fazer a insercao dos dados na base de dados
         $contents->save();
