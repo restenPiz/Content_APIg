b36aa985f6ce44f059ce4afb454e1c29c5a0f4f6
restenPiz <99766054+restenPiz@users.noreply.github.com>
2023-10-03 09:24:34 +0200

    Some updates in the application

--- content_api/routes/api.php
+++ content_api/routes/api.php
@@ -17,3 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+//*Inicio das rotas da api
+Route::get('/dashboard', [dashboardController::class, 'index'])->middleware(['auth'])->name('dashboard');
+
